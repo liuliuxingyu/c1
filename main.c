405d09bc7ddee860a832c2f244fda4d11f0e4447
邵楠 <1985890648@qq.com>
2019-01-15 11:45:48 +0800

    构建钱包系统框架

--- main.c
+++ main.c
@@ -2,7 +2,29 @@
 
 int main(){
 
-    printf("hello word!");
+    printf("jinruqianbaoguanlixitong\n");
+    while(1){
+        printf("1---cunqian\n ");
+        printf("2---quqian\n");
+        printf("3---yue\n");
+        printf("4---tuichu\n");
+        printf("qingshuruxiangyingdegongnengbianhao:\n");
+        int code;
+        scanf("%d",&code);
+        if(code==1){
+
+        }
+        if(code==2){
+
+        }
+        if(code==3){
+
+        }
+        if(code==4){
+            
+        }
+
+    }
 
     return 0;
 
